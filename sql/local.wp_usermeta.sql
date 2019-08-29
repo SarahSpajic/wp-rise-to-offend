3876db2ed833b134df0fe79855f4585feda83744
Sarah Spajic <39314999+SarahSpajic@users.noreply.github.com>
2019-08-29 07:39:32 -0700

    Add files via upload

---
+++ sql/local.wp_usermeta.sql
@@ -0,0 +1,25 @@
+/*!40101 SET NAMES binary*/;
+/*!40014 SET FOREIGN_KEY_CHECKS=0*/;
+/*!40103 SET TIME_ZONE='+00:00' */;
+INSERT INTO `wp_usermeta` VALUES
+(1,1,"nickname","sarahw828"),
+(2,1,"first_name",""),
+(3,1,"last_name",""),
+(4,1,"description",""),
+(5,1,"rich_editing","true"),
+(6,1,"syntax_highlighting","true"),
+(7,1,"comment_shortcuts","false"),
+(8,1,"admin_color","fresh"),
+(9,1,"use_ssl","0"),
+(10,1,"show_admin_bar_front","true"),
+(11,1,"locale",""),
+(12,1,"wp_capabilities","a:1:{s:13:\"administrator\";b:1;}"),
+(13,1,"wp_user_level","10"),
+(14,1,"dismissed_wp_pointers","theme_editor_notice"),
+(15,1,"show_welcome_panel","1"),
+(16,1,"session_tokens","a:1:{s:64:\"46faf8011bb6fdff089ea070cde3482b2509a6a86fddfd1e56b298ee1503d8db\";a:4:{s:10:\"expiration\";i:1566490565;s:2:\"ip\";s:9:\"127.0.0.1\";s:2:\"ua\";s:135:\"Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1\";s:5:\"login\";i:1566317765;}}"),
+(17,1,"wp_dashboard_quick_press_last_post_id","4"),
+(18,1,"community-events-location","a:1:{s:2:\"ip\";s:12:\"192.168.95.0\";}"),
+(19,1,"managenav-menuscolumnshidden","a:4:{i:0;s:11:\"link-target\";i:1;s:15:\"title-attribute\";i:2;s:3:\"xfn\";i:3;s:11:\"description\";}"),
+(20,1,"metaboxhidden_nav-menus","a:1:{i:0;s:12:\"add-post_tag\";}"),
+(21,1,"nav_menu_recently_edited","2");
---
+++ sql/local.wp_users.sql
@@ -0,0 +1,5 @@
+/*!40101 SET NAMES binary*/;
+/*!40014 SET FOREIGN_KEY_CHECKS=0*/;
+/*!40103 SET TIME_ZONE='+00:00' */;
+INSERT INTO `wp_users` VALUES
+(1,"sarahw828","$P$Bhgnyogl8MUUqJu4dzu7h59IlTv6Yb0","sarahw828","sarahw828@aol.com","","2019-08-14 18:55:27","",0,"sarahw828");
